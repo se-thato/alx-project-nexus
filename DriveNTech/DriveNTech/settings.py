43734e4507d1923bf8ba73f18f9b61676a0bad73
se-thato <thatoselepe80@gmail.com>
2025-08-12 17:35:16 +0200

    CSRF_TRUSTED_ORIGINS

--- DriveNTech/DriveNTech/settings.py
+++ DriveNTech/DriveNTech/settings.py
@@ -250,7 +250,7 @@ CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND")
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
-EMAIL_PORT = os.environ.get("EMAIL_PORT", cast=int)
+EMAIL_PORT = config("EMAIL_PORT", cast=int, default=587)
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", cast=bool)
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
