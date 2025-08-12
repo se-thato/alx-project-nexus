b758f82b28298919c3b3f145a3fdc531f602ee4b
se-thato <thatoselepe80@gmail.com>
2025-08-12 17:25:08 +0200

    CSRF_TRUSTED_ORIGINS

--- DriveNTech/DriveNTech/settings.py
+++ DriveNTech/DriveNTech/settings.py
@@ -15,7 +15,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = os.environ("CSRF_TRUSTED_ORIGINS")
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS")
 
 
 # Application definition
